0355293e1ece810b19d436d6b1f01d07be1597d0
lawrence1986 <madusquare@gmail.com>
2023-10-23 08:32:32 -0700

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Accessing a REST API for todo lists of employees"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    url = f"{base_url}/{employeeId}"
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todo_url = f"{url}/todos"
+    response = requests.get(todo_url)
+    tasks = response.json()
+
+    with open(f"{employeeId}.csv", 'w') as file:
+        for task in tasks:
+            file.write(f'"{employeeId}","{username}","{
+			    task.get("completed")}","{task.get("title")}"\n')
